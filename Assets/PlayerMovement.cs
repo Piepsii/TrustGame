1c8a2664eded0b79665a6a7407e7ac8c2c5784f6
Paul Brandstetter <paulbrandstetter@tuta.io>
2023-02-07 10:43:50 +0100

    New Tileset

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -10,6 +10,7 @@ public class PlayerMovement : MonoBehaviour
     public Transform movePoint;
 
     public LayerMask whatStopsMovement;
+    public bool secondPlayer = false;
 
     // Start is called before the first frame update
     void Start()
@@ -22,20 +23,33 @@ public class PlayerMovement : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if(Vector3.Distance(transform.position, movePoint.position) <= 0.5f)
+        if (secondPlayer)
         {
-            if(Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
+            GetMovement("Horizontal2", "Vertical2");
+
+        }
+        else
+        {
+            GetMovement("Horizontal", "Vertical");
+        }
+    }
+
+    private void GetMovement(string horizontal, string vertical)
+    {
+        if (Vector3.Distance(transform.position, movePoint.position) <= 0.5f)
+        {
+            if (Mathf.Abs(Input.GetAxisRaw(horizontal)) == 1f)
             {
-                if(!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .2f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(Input.GetAxisRaw(horizontal), 0f, 0f), .2f, whatStopsMovement))
                 {
-                    movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                    movePoint.position += new Vector3(Input.GetAxisRaw(horizontal), 0f, 0f);
                 }
             }
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
+            if (Mathf.Abs(Input.GetAxisRaw(vertical)) == 1f)
             {
-                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .2f, whatStopsMovement))
+                if (!Physics2D.OverlapCircle(movePoint.position + new Vector3(0f, Input.GetAxisRaw(vertical), 0f), .2f, whatStopsMovement))
                 {
-                    movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                    movePoint.position += new Vector3(0f, Input.GetAxisRaw(vertical), 0f);
                 }
             }
         }
